4a08cecb9a127b6ab1d39f8f38637c17957bf417
Andrey Mileshko <AndreyPrograming.Mileshko@yandex.ru>
2024-10-16 17:53:47 +0700

    preliminary result

--- src/main/java/ru/netology/initializer/ApplicationInitializer.java
+++ src/main/java/ru/netology/initializer/ApplicationInitializer.java
@@ -1,12 +1,14 @@
 package ru.netology.initializer;
 
+import jakarta.servlet.ServletContext;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletContext;
+
 
 public class ApplicationInitializer implements WebApplicationInitializer {
+
     @Override
     public void onStartup(ServletContext servletContext) {
         final var context = new AnnotationConfigWebApplicationContext();
