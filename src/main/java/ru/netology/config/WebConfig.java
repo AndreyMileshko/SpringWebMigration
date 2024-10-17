f65d05ad50ed8e19b64e81e54399baac2f43a023
Andrey Mileshko <AndreyPrograming.Mileshko@yandex.ru>
2024-10-18 00:29:47 +0700

    JDK 17 error

--- src/main/java/ru/netology/config/WebConfig.java
+++ src/main/java/ru/netology/config/WebConfig.java
@@ -3,14 +3,23 @@ package ru.netology.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
+@EnableWebMvc
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
     @Bean
     public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
         final var adapter = new RequestMappingHandlerAdapter();
         adapter.getMessageConverters().add(new GsonHttpMessageConverter());
         return adapter;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 }
--- src/main/java/ru/netology/initializer/ApplicationInitializer.java
+++ src/main/java/ru/netology/initializer/ApplicationInitializer.java
@@ -1,10 +1,12 @@
 package ru.netology.initializer;
 
-import jakarta.servlet.ServletContext;
+
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 
 public class ApplicationInitializer implements WebApplicationInitializer {
